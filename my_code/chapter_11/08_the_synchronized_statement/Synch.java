c077c619bc32c7c62c620111a2b890906656f56a
ordinary-developer <merely.ordinary.developer@gmail.com>
2015-07-20 13:28:13 +0400

    add some code snippets about synchronization
    add some code snippets about synchronization in java,
    and add a synopsis dealed with syncrhonization

---
+++ my_code/chapter_11/08_the_synchronized_statement/Synch.java
@@ -0,0 +1,57 @@
+class Callme {
+    public void call(String message) {
+        System.out.print("[" + message);
+        try {
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println("]");
+    }
+}
+
+class Caller implements Runnable {
+    public Caller(Callme target, String message) {
+        this.target = target;
+        this.message = message;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public void run() {
+        synchronized(target) { 
+            target.call(message);
+        }
+    }
+
+    public Thread getThread() {
+        return thread;
+    }
+
+    public void setThread(Thread thread) {
+        this.thread = thread;
+    }
+
+    private String message;
+    private Callme target;
+    private Thread thread;
+}
+
+class Synch {
+    public static void main(String args[]) {
+        Callme target = new Callme();
+        Caller ob1 = new Caller(target, "Hello");
+        Caller ob2 = new Caller(target, "Synchronized");
+        Caller ob3 = new Caller(target, "World");
+
+        try {
+            ob1.getThread().join();
+            ob2.getThread().join();
+            ob3.getThread().join();
+        }
+        catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+    }
+}
