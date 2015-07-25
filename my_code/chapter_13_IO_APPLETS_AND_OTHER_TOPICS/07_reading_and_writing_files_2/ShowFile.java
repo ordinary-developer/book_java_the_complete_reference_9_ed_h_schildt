21c044fd4b8755e7aee8aa5d0fbff7814d782d64
ordinary-developer <merely.ordinary.developer@gmail.com>
2015-07-25 15:55:07 +0400

    add all about streams from the chapter 13
    add all (code samples and a synosis) about stream
    (input, output from console and files) from the chapter 13

---
+++ my_code/chapter_13_IO_APPLETS_AND_OTHER_TOPICS/07_reading_and_writing_files_2/ShowFile.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+class ShowFile {
+    public static void main(String args[]) {
+        int i;
+        FileInputStream fin = null;
+
+        if (args.length != 1) {
+            System.out.println("Usage: ShowFile filename");
+            return;
+        }
+
+        try {
+            fin = new FileInputStream(args[0]);
+
+            do {
+                i = fin.read();
+                if (i != -1) {
+                    System.out.print((char) i);
+                }
+            } while (i != -1);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+        catch (IOException e) {
+            System.out.println("An I/O Error Occurred");
+        }
+        finally {
+            try {
+                if (fin != null) {
+                    fin.close();
+                }
+            }
+            catch (IOException e) {
+                System.out.println("Error closing file");
+            }
+        }
+
+    }
+}
